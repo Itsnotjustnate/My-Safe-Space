c390fcc0e0c10da8bc7ad963c9e0f727117dec6c
Nathan Eduvala <76193599+Itsnotjustnate@users.noreply.github.com>
2022-09-20 16:47:41 -0700

    Delete src directory

--- src/components/FooterComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Footer (){
-    return (
-        <div>This is some bottom shelf safe space.</div>
-    )
-}
-
-export default Footer;
--- src/components/HeaderComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Header (){
-    return (
-        <div>This is top shelf safe space.</div>
-    )
-}
-
-export default Header;
